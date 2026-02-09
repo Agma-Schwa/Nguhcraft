31a32a6c82812e82fbccb28ed9f4ab3dcd357e9e
Xhesas <namnak.xhesas@gmail.com>
2026-02-09 01:41:48 +0100

    updated froglight item tag

--- src/main/java/org/nguh/nguhcraft/data/NguhcraftDataGenerator.kt
+++ src/main/java/org/nguh/nguhcraft/data/NguhcraftDataGenerator.kt
@@ -225,6 +225,8 @@ class NguhcraftItemTagProvider(
             .add(Blocks.OCHRE_FROGLIGHT.asItem())
             .add(Blocks.VERDANT_FROGLIGHT.asItem())
             .add(Blocks.PEARLESCENT_FROGLIGHT.asItem())
+            .add(NguhBlocks.AZURE_FROGLIGHT.asItem())
+            .add(NguhBlocks.SANGUINE_FROGLIGHT.asItem())
     }
 }
 
